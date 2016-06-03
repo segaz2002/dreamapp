2316b23781f59b08fa7073e7183ae4e829c06678
gabriel <segaz2002@gmail.com>
2016-06-03 08:45:25 +0300

    Start of, basic setup and others

---
+++ AcademicYear.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabriel
+ * Date: 5/31/16
+ * Time: 8:14 PM
+ */
+
+class AcademicYear {
+    private $session;
+    private $terms = array();
+    private $startDay;
+
+    public function __construct($sessionData){
+        $this->session = array_keys($sessionData)[0];
+        foreach($sessionData as $k => $v){
+            if($k == 'start'):
+                $this->startDay = $v;
+            else:
+                $this->terms[$k] = $v;
+            endif;
+        }
+    }
+
+    public function getSession(){
+        return $this->session;
+    }
+
+    public function getTerms(){
+        return $this->terms;
+    }
+
+    public function getStartDate(){
+        return $this->startDay;
+    }
+
+    public function getSessionFromDate($date){
+        $date = new ExpressiveDate;
+    }
+
+    /**
+     * Check wether a date fall between the terms of this academic year
+     * @param $date
+     * @return bool
+     */
+    public function isWithin($date){
+        echo $date;
+        $queryDate = new ExpressiveDate($date);
+        foreach($this->terms as $term ){
+            $termPeriod = explode(':',$term);
+            $beginDate = new ExpressiveDate($termPeriod[0]);
+            $endDate = new ExpressiveDate($termPeriod[1]);
+            echo $beginDate .'\n';
+            echo $endDate .'\n';
+            var_dump($queryDate->greaterOrEqualTo($beginDate) && $queryDate->lessOrEqualTo($endDate));
+            if($queryDate->greaterOrEqualTo($beginDate) && $queryDate->lessOrEqualTo($endDate)){
+                return true;
+                break;
+            }
+        }
+    }
+
+}
+
---
+++ main.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabriel
+ * Date: 5/31/16
+ * Time: 6:53 PM
+ */
+require_once('AcademicYear.php');
+require 'vendor/autoload.php';
+$sessionCache = [];
+$configData = parse_ini_file("data.ini",true);
+
+foreach($configData as $k => $v){
+    $sessionCache[$k] = new AcademicYear($configData[$k]);
+}
+var_dump($sessionCache);
+//
+//$ac = array(
+//    '2016/17' => array(
+//        'start' => 'September 1',
+//        "First trimester" =>"September 1 : October 28",
+//        "Second trimester"=>"November 1 : January 20",
+//        "Third trimester" =>"January 28 : April 19"
+//    )
+//);
+//
+//$a = new AcademicYear($ac);
+//echo $a->getSession()."\n";
+//echo $a->getStartDate()."\n";
+//var_dump($a->getTerms());
+
+//echo normalizeDate('12-06-2006');
+/**
+ * @param $date e.g dd-mm-yyyy
+ */
+
+if($argv[1]){
+    foreach($sessionCache as $ac){
+        if($ac->isWithin(normalizeDate($argv[1]))){
+            echo $ac->session;
+        }else{
+            echo "There is no macth\n";
+        }
+    }
+}
+
+
+function normalizeDate($date){
+    $dateSegment = explode('-',$date);
+    if(checkdate($dateSegment[1],$dateSegment[0],$dateSegment[2])){
+        $months = array(
+            1 => "January",
+            2 => "Febuary",
+            3 => "March",
+            4 => "April",
+            5 => "May",
+            6 => "June",
+            7 => "July",
+            8 => "August",
+            9 => "September",
+            10 => "October",
+            11 => "November",
+            12 => "December");
+        return $months[(int)$dateSegment[1]]." ".$dateSegment[0].",".$dateSegment[2];
+    }else{
+        echo "Incorrect date, enter date in this format dd-mm-yyyy";
+        exit;
+    }
+
+
+}
